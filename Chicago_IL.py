7a78826c12017d1beeef29f0b4557b3af70fa2e2
honeysingh1 <singh178@purdue.edu>
2014-10-13 21:30:37 -0400

    Edited script

--- Chicago_IL.py
+++ Chicago_IL.py
@@ -1,24 +1,45 @@
+#author: Honey Singh
+#10/13/2014
 
+#Import ftplib to transfer file from CDoT
 from ftplib import FTP
 import sys
 import string
-#from StringIO import StringIO
 
 def finding():
 	ftps = FTP('ftp.travelmidwest.com')
+	# opening ftp
 	ftps.login('purdueececam2','ca%m2$pur&du9e')
-#ftps.retrlines('LIST')
-	r = open('filedata','w') 
-	ftps.retrbinary('RETR /cameraInfo.txt', r.write)
+	rptr = open('filedata','w') 
+	#writing the data into a local file
+	ftps.retrbinary('RETR /cameraInfo.txt', rptr.write)
 	ne = open('filedata','r')
+	# opening the data file 
 	test = ne.readlines()
+	# reading each line ending with '\n'
 	f = open('Chicago_IL','w')
-
+	ls = [] # creating a list to store each tuple
+	count = 0
 	for i in range(0,len(test)-1):
-		test[i] = test[i].replace('|','#').replace('\n','#IL#USA\n')
-		f.write("ftp://ftp.travelmidwest.com/"+test[i])
+		k = 0
+		le = len(test[i])
+		while k < le :
+			# looping to partition data 
+			t = test[i].partition("|")
+		# adding partitioned data to list
+			ls.append(t[0])
+			k = k + 1
+		# moving on to next part of string to partition
+			test[i] = t[2]
+		ls[2] = float(ls[2])/100000
+		# original data did not have decimal
+		ls[3] = float(ls[3])/100000
+		# writing each element of tuple saved in list
+		f.write(ls[1]+"#Chicago#IL#USA#"+"ftp://ftp.travelmidwest.com/"+ls[0]+"#"+str(ls[2])+"#"+str(ls[3])+"\n")
+		
+		count = 0
 
-	r.close()
+	rptr.close()
 	ne.close()
 	f.close()
 finding()
